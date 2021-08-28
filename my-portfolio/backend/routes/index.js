e1a5e1a6f8df3f7dd5b57e7ab28026c3e7312244
Bruno E. M. Ferreira <brunoeduardo_ferreira@hotmail.com>
2021-08-28 18:56:21 -0300

    Aula 11 - Conectando ao MongoDB com Mongoose

--- my-portfolio/backend/routes/index.js
+++ my-portfolio/backend/routes/index.js
@@ -1,11 +1,14 @@
+// Arquivo index de Rotas
 const router = require('express').Router();
+// disponiobiliza a conexão do Mongo para esse arquivo
+require('../db/mongoConnection');
 
 const portfolio = require('./portfolio');
 
 router.get('/', (req, res) => {
   res.json({
     success: false,
-    message: 'Please, do not call /api, it is for our server  '
+    message: 'Please, do not call /api, it is for our server'
   })
 });
 
--- my-portfolio/server.js
+++ my-portfolio/server.js
@@ -7,8 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-
-
 app.get('/', (req, res) => {
   res.json({
     'success': true
